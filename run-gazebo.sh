1ba8004f59de16b7308ca81b88cf85b7734962d9
Anoop-Grunt <anoopv4952@gmail.com>
2025-11-04 15:26:24 -0800

    script to bridge

--- run-gazebo.sh
+++ run-gazebo.sh
@@ -1,18 +1,20 @@
 xhost +local:docker
 docker run -ti \
-  --rm\
+  --rm \
   -u root:root \
-  --name gazebo-sim\
+  --name gazebo-sim \
   --group-add video \
   --device /dev/dri:/dev/dri \
   -e DISPLAY=$DISPLAY \
-  -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  -e XDG_RUNTIME_DIR=/run/user/0 \
   -e QT_X11_NO_MITSHM=1 \
   -e MESA_GL_VERSION_OVERRIDE=4.5 \
   -e MESA_GLSL_VERSION_OVERRIDE=450 \
   -e LIBGL_ALWAYS_INDIRECT=0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR \
   -v $(pwd)/sim_and_bridge_clean.launch.py:/tmp/sim_and_bridge.launch.py:ro \
   gazebo-with-ros \
-  bash -c "source /opt/ros/rolling/setup.bash && ros2 launch /tmp/sim_and_bridge.launch.py > /tmp/gazebo.log 2>&1 & bash"
+  bash -c "mkdir -p /run/user/0 && chmod 0700 /run/user/0 && \
+           source /opt/ros/rolling/setup.bash && \
+           ros2 launch /tmp/sim_and_bridge.launch.py > /tmp/gazebo.log 2>&1 & \
+           bash"
